b5b6ea7fb3463f51b7dff98a49f02bb258b151a6
Volkov Evgeny <volkov.evgeny@huawei.com>
2020-04-15 17:47:11 +0300

    delelte outer loop

--- C.v3/FFT_OpenACC_iterative.c
+++ C.v3/FFT_OpenACC_iterative.c
@@ -1,5 +1,8 @@
 /*+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-*	To compile: pgcc -acc -ta=tesla -Minfo=accel FFT_OpenACC.c
+*	To compile: 
+*	pgcc -acc -ta=tesla -Minfo=accel FFT_OpenACC_iterative.c
+*	pgcc -acc -ta=multicore -Minfo=accel FFT_OpenACC_iterative.c
+*	pgcc -acc -ta=host -Minfo=accel FFT_OpenACC_iterative.c
 +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+*/
 
 #include <stdio.h>
@@ -16,7 +19,7 @@ int main()
 {
 	const int n = 2048;
 	const int m = 2048;
-	const int l = 50;
+	// const int l = 50;
 	const int log2n = log2f(m);
 
 	// Declaring input, output arrays
@@ -43,55 +46,55 @@ int main()
 
 	clock_t begin = clock();
 
-// FFT
-	#pragma acc declare copyin(dataIn, n, m, log2n) copyout(dataOut)
-	#pragma acc parallel loop
+	// FFT
+#pragma acc declare copyin(dataIn, n, m, log2n) copyout(dataOut)
+#pragma acc parallel loop
 	// #pragma acc kernels
-	for (int count = 0; count < l; ++count)
+	// for (int count = 0; count < l; ++count)
+	// {
+	for (int idx = 0; idx < n; ++idx)
 	{
-		for (int idx = 0; idx < n; ++idx)
+		// bit reversal of the given array
+		for (int j = 0; j < m; ++j)
 		{
-			// bit reversal of the given array
-			for (int j = 0; j < m; ++j)
+			int newN = 0;
+			int x = j;
+			for (int i1 = 0; i1 < log2n; i1++)
 			{
-				int newN = 0;
-				int x = j;
-				for (int i1 = 0; i1 < log2n; i1++)
-				{
-					newN <<= 1;
-					newN |= (x & 1);
-					x >>= 1;
-				}
-				int rev = newN;
-				dataOut[idx][j] = dataIn[idx][rev];
+				newN <<= 1;
+				newN |= (x & 1);
+				x >>= 1;
 			}
-			const cplx J = I;
+			int rev = newN;
+			dataOut[idx][j] = dataIn[idx][rev];
+		}
+		const cplx J = I;
 
-			for (int s = 1; s <= log2n; ++s)
+		for (int s = 1; s <= log2n; ++s)
+		{
+			int m1 = 1 << s;  // 2 power s
+			int m2 = m1 >> 1; // m2 = m/2 -1
+			cplx w = 1;
+			cplx wm = cexp(J * (PI / m2));
+			for (int j = 0; j < m2; ++j)
 			{
-				int m1 = 1 << s;  // 2 power s
-				int m2 = m1 >> 1; // m2 = m/2 -1
-				cplx w = 1;
-				cplx wm = cexp(J * (PI / m2));
-				for (int j = 0; j < m2; ++j)
+				for (int k = j; k < m; k += m1)
 				{
-					for (int k = j; k < m; k += m1)
-					{
-						// t = twiddle factor
-						cplx t = w * dataOut[idx][k + m2];
-						cplx u = dataOut[idx][k];
+					// t = twiddle factor
+					cplx t = w * dataOut[idx][k + m2];
+					cplx u = dataOut[idx][k];
 
-						// similar calculating y[k]
-						dataOut[idx][k] = u + t;
+					// similar calculating y[k]
+					dataOut[idx][k] = u + t;
 
-						// similar calculating y[k+n/2]
-						dataOut[idx][k + m2] = u - t;
-					}
-					w *= wm;
+					// similar calculating y[k+n/2]
+					dataOut[idx][k + m2] = u - t;
 				}
+				w *= wm;
 			}
 		}
 	}
+	// }
 
 	clock_t end = clock();
 	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
