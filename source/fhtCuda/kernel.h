c22cd4a0fc6dcfa4381e773fb0b7ea06fe02bcc4
Volkov Evgeny <football.volkov@mail.ru>
2023-02-22 13:55:29 +0300

    refactor of project structure

---
+++ source/fhtCuda/kernel.h
@@ -0,0 +1,9 @@
+#ifndef KERNEL_CUH_
+#define KERNEL_CUH_
+
+//using DataType = unsigned __int8;
+using DataType = float;
+
+void matrixMultiplication(DataType *A, DataType *B, DataType *C, int N);
+
+#endif
