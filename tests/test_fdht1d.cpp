cdce50f2733fb8ad96a201062ee9a628c05c0988
22dla <volkov22dla@yandex.ru>
2024-08-09 17:46:18 +0300

    cols заменены на rows

--- tests/test_fdht1d.cpp
+++ tests/test_fdht1d.cpp
@@ -5,31 +5,30 @@
 #include <numeric>
 #include <cstring>
 
-
 int main(int argc, char** argv) {
-	size_t rows = static_cast<size_t>(pow(2, 19));
+	size_t cols = static_cast<size_t>(pow(2, 2));
 	RapiDHT::Modes mode = RapiDHT::CPU;
 
 	// Обрабатываем аргументы командной строки, если они есть
 	auto args_map = parseCommandLine(argc, argv);	
-	rows = parseSize(args_map, "--rows").value_or(rows);
+	cols = parseSize(args_map, "--cols").value_or(cols);
 	mode = parseMode(args_map).value_or(mode);
 
 	// Выводим полученные значения
-	std::cout << "Rows: " << rows << std::endl;
+	std::cout << "Cols: " << cols << std::endl;
 	std::cout << "Mode: " << (mode == RapiDHT::CPU ? "CPU" :
-		mode == RapiDHT::GPU ? "GPU" : "RFFT") << std::endl;
+		mode == RapiDHT::GPU ? "GPU" : "RFFT") << std::endl << std::endl;
 
 	// Задаем начальные данные
-	auto a1_1 = makeData<double>({ rows });
-	auto a1_2 = makeData<double>({ rows });
+	auto a1_1 = makeData<double>({ cols }); // Данные, которые будем преобразовывать
+	auto a1_2 = makeData<double>({ cols }); // Для подсчета ошибки
 	//printData1D(a1);
 
 	double common_start, common_finish;
 	common_start = clock() / static_cast<double>(CLOCKS_PER_SEC);
 
 	// Считываем преобразование Харли
-	RapiDHT::HartleyTransform ht(rows, 0, 0, mode);
+	RapiDHT::HartleyTransform ht(cols, 0, 0, mode);
 	ht.ForwardTransform(a1_1);
 	ht.InverseTransform(a1_1);
 
