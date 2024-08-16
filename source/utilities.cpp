e8d5f5a86774b1482e8964cac217521ce6a6463d
22dla <volkov22dla@yandex.ru>
2024-08-16 12:50:45 +0300

    приведены к матлабу make_data

--- source/utilities.cpp
+++ source/utilities.cpp
@@ -81,10 +81,11 @@ std::vector<T> make_data(std::initializer_list<int> sizes) {
 		data.resize(data.size() * dim_sizes[i]);
 	}
 	// fill massive with random values
+#pragma omp parallel for
 	for (int idx = 0; idx < data.size(); ++idx) {
-		data[idx] = static_cast<T>(dim_sizes[0] + std::cos(std::asin(0.1) / (idx + 1)) -
-			std::sin(std::cos(idx / dim_sizes[0])) +
-			std::tan(idx * dim_sizes[0]) + 2 + idx) / (dim_sizes[0] * dim_sizes[0]);
+		data[idx] = static_cast<T>(static_cast<double>(dim_sizes[0]) + std::cos(std::asin(0.1) / (static_cast<double>(idx) + 1)) -
+			std::sin(std::cos(static_cast<double>(idx) / static_cast<double>(dim_sizes[0]))) +
+			std::tan(static_cast<double>(idx * dim_sizes[0])) + static_cast<double>(2 + idx)) / static_cast<double>((dim_sizes[0] * dim_sizes[0]));
 	}
 	//std::iota(data.begin(), data.end(), 0);
 
